248b642f0d08717985ac8fa75190326c2a56a1e1
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-06-10 12:37:57 -0600

    fix 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,38 +1,19 @@
 #!/usr/bin/node
-// import request module
+
 const request = require('request');
-
-// get the URL to process data from
 const url = process.argv[2];
-
-// make a get request to the provided urel
 request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-
-  // parse the json string into an object
-  const todosData = JSON.parse(body);
-  const completedTasks = {};
-
-  // iterate through the data
-  for (const task of todosData) {
-    // get the userId
-    const userId = task.userId;
-    // get the boolean value of the task completion status
-    const isCompleted = task.completed;
-
-    // initialize the object for the current userId
-    if (!completedTasks[userId]) {
-      completedTasks[userId] = 0;
+  if (error) console.log(error);
+  const data = JSON.parse(body);
+  const todoList = {};
+  data.forEach(todo => {
+    if (todo.completed) {
+      if (todoList[todo.userId]) {
+        todoList[todo.userId]++;
+      } else {
+        todoList[todo.userId] = 1;
+      }
     }
-
-    // if completed is true, increment the number of completed task
-    if (isCompleted === true) {
-      completedTasks[userId]++;
-    }
-  }
-
-  // print the results
-  console.log(completedTasks);
+  });
+  console.log(todoList);
 });
